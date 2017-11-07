32636b8f27f8111423190fb448542b4704dc324f
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-11-07 21:31:15 +0800

    feat: init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { send } = require('micro');
+const { router, get } = require('microrouter');
+const puppeteer = require('puppeteer');
+
+const handler = async (req, res) => {
+  const { url, selector } = req.query;
+  let browser;
+  try {
+    browser = await puppeteer.launch({
+      headless: false,
+      // slowMo: 20,
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+    const content = await page.$eval(selector, el => el.innerHTML);
+    send(res, 200, content);
+  } catch (error) {
+    console.log(error); //eslint-disable-line
+    send(res, 400, error);
+  }
+
+  browser.close();
+};
+
+module.exports = router(get('/api', handler));
---
+++ tasks/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+
+# ENV Variables, Note: NOW_TOKEN in travisCI
+# NOW config
+TEAM='website-api'
+PROJECT='micro-website-api'
+ALIAS='micro-website-api.now.sh'
+
+export PATH="./node_modules/.bin:$PATH"
+
+# 1. Wair for deployment ready
+URL=$(now --public --token "$NOW_TOKEN" --team $TEAM)
+await-url "$URL"
+now ls --token "$NOW_TOKEN" --team $TEAM
+
+# 2. Alias
+now alias set "$URL" "$ALIAS" --token "$NOW_TOKEN" --team $TEAM
+
+# 3. Purge old services
+now remove --yes --safe --token "$NOW_TOKEN" --team $TEAM $PROJECT
+
+# 4. Scale to 1
+now scale "$ALIAS" 1 --token "$NOW_TOKEN" --team $TEAM
+
+# 5. Log results
+now ls --token "$NOW_TOKEN" --team $TEAM
+now alias ls --token "$NOW_TOKEN" --team $TEAM
