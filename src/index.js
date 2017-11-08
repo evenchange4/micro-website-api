c28e506eb56808904dbad0b13797c958717a697e
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-11-08 10:27:57 +0800

    feat: multiple action

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
-const { send } = require('micro');
+const { send, createError } = require('micro');
 const { router, get } = require('microrouter');
+const Promise = require('bluebird');
 const puppeteer = require('puppeteer');
 
 const handler = async (req, res) => {
-  const { url, selector } = req.query;
   let browser;
+
   try {
+    // 1. Parameter check
+    const { url, selector, actions } = req.query;
+    if (!url) throw createError(400, `query parameter 'url' is required.`);
+    if (!selector) {
+      throw createError(400, `query parameter 'selector' is required.`);
+    }
+
     browser = await puppeteer.launch({
       // headless: false,
       // slowMo: 20,
       args: ['--no-sandbox'], // for docker
     });
     const page = await browser.newPage();
-    await page.goto(url);
+
+    // 2. Goto
+    await page.goto(url, {
+      timeout: 9999999,
+    });
+
+    // 3. Action
+    if (actions) {
+      await Promise.each(
+        typeof actions === 'string' ? Array.of(actions) : actions,
+        async action => {
+          const [click, wait] = action.split(',');
+          if (click) {
+            await page.click(click);
+          }
+          if (wait) {
+            await page.waitForSelector(wait, {
+              timeout: 9999999,
+            });
+          }
+        },
+      );
+    }
+
+    // 4. Selector
     const content = await page.$eval(selector, el => el.innerHTML);
+
     send(res, 200, content);
   } catch (error) {
     console.log(error); //eslint-disable-line
