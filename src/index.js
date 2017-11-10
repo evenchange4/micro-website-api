ea87083a0ed3dfca7c7c7901d56cf62be50b8033
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-11-10 22:48:55 +0800

    fix(web): disalbe sw

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './web/components/App';
-import registerServiceWorker from './web/utils/registerServiceWorker';
+// import registerServiceWorker from './web/utils/registerServiceWorker';
 import autotrack from './web/utils/autotrack';
 
 import './web/utils/style';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
 autotrack(process.env.REACT_APP_GA_ID);
