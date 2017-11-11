79fd3ef9a05b1079d78349dad357440373f135ac
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-11-11 14:31:31 +0800

    refactor(es): rx esmodule

---
+++ src/web/utils/rxEsmodule.js
@@ -0,0 +1,13 @@
+/**
+ * ref: https://github.com/ReactiveX/rxjs/blob/master/doc/lettable-operators.md#build-and-treeshaking
+ */
+export { mapTo } from 'rxjs/operators/mapTo';
+export { map } from 'rxjs/operators/map';
+export { scan } from 'rxjs/operators/scan';
+export { startWith } from 'rxjs/operators/startWith';
+export { withLatestFrom } from 'rxjs/operators/withLatestFrom';
+export { tap } from 'rxjs/operators/tap';
+export { filter } from 'rxjs/operators/filter';
+export { switchMap } from 'rxjs/operators/switchMap';
+export { combineLatest } from 'rxjs/operators/combineLatest';
+export { merge } from 'rxjs/observable/merge';
