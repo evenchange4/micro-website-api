a8286e95cebf2f43595945ff8475382bde5ba497
Michael Hsu <evenchange4@gmail.com>
2017-11-15 13:18:27 +0800

    fix(Web): remove GA (#14)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './web/components/App';
 // import registerServiceWorker from './web/utils/registerServiceWorker';
 import autotrack from './web/utils/autotrack';
-
 import './web/utils/style';
 
+// TODO: make ga configurable?
+const GA_ID =
+  window.location.host === 'micro-website-api.now.sh' && 'UA-33845990-13';
+
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-autotrack(process.env.REACT_APP_GA_ID);
+autotrack(GA_ID);
